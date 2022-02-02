ecfce1c2855d8ee2b2952e8140637397d60d9219
Sarthak Agrawal <agraw189@umn.edu>
2022-02-02 08:40:42 -0600

    Initial Commit

---
+++ Find My EP/Find_My_EPApp.swift
@@ -0,0 +1,17 @@
+//
+//  Find_My_EPApp.swift
+//  Find My EP
+//
+//  Created by 64000774 on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Find_My_EPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
