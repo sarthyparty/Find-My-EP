ccfd7065861d98737e77784b91b1a05e1aa7fd88
ViswaNadimpalli <64009170@ep-student.org>
2022-02-11 09:18:55 -0600

    Updated Basic UI

--- Find My EP/ContentView.swift
+++ Find My EP/ContentView.swift
@@ -15,32 +15,43 @@ struct ContentView: View {
     @State private var inters = ""
     @State private var options = []
     var body: some View {
-        VStack(alignment: .leading) {
-            TextField("Enter start ID (0-5)", text: $startID)
-            TextField("Enter end ID (0-5)", text: $endID)
-            Button("Calculate") {
-                let res = school.findPath(start: rooms[Int(startID) ?? 0], end: rooms[Int(endID) ?? 5])
-                dist = res.dist
-                hallways = ""
-                for hall in res.halls {
-                    hallways+=String(hall.id) + ", "
+        ZStack {
+            Color .white
+                .edgesIgnoringSafeArea(.all)
+            VStack(alignment: .leading) {
+                TextField("Enter start ID (0-5)", text: $startID)
+                    .multilineTextAlignment(.center)
+                TextField("Enter end ID (0-5)", text: $endID)
+                    .multilineTextAlignment(.center)
+                Button("Calculate") {
+                    let res = school.findPath(start: rooms[Int(startID) ?? 0], end: rooms[Int(endID) ?? 5])
+                    dist = res.dist
+                    hallways = ""
+                    for hall in res.halls {
+                        hallways+=String(hall.id) + ", "
+                    }
+                    hallways = "Hallway IDs (Reversed): " + hallways
+                    inters = ""
+                    for inter in res.inters {
+                        inters+=String(inter) + ", "
+                    }
+                    inters = "Intersection IDs: " + inters
+                    print(school.halls)
+                    
                 }
-                hallways = "Hallway IDs (Reversed): " + hallways
-                inters = ""
-                for inter in res.inters {
-                    inters+=String(inter) + ", "
-                }
-                inters = "Intersection IDs: " + inters
-                print(school.halls)
-                
+                .frame(maxWidth: .infinity)
+                Image("Map_GPS")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text(String(dist))
+                    .frame(maxWidth: .infinity, alignment: .center)
+                Text(hallways)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                Text(inters)
+                    .frame(maxWidth: .infinity, alignment: .center)
             }
-            Image("Map_GPS")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-            Text(String(dist))
-            Text(hallways)
-            Text(inters)
         }
+        
     }
 }
 
