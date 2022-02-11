3c40af15b6501547962f2f09a11743e5f23dcd5b
Sarthak Agrawal <agraw189@umn.edu>
2022-02-11 10:24:00 -0600

    fixed color stuff

--- Find My EP/ContentView.swift
+++ Find My EP/ContentView.swift
@@ -16,15 +16,19 @@ struct ContentView: View {
     @State private var options = []
     var body: some View {
         ZStack {
-            Color .white
-                .edgesIgnoringSafeArea(.all)
             VStack(alignment: .leading) {
-                TextField("Enter start ID (0-5)", text: $startID)
+                TextField("Enter start room", text: $startID)
                     .multilineTextAlignment(.center)
-                TextField("Enter end ID (0-5)", text: $endID)
+                TextField("Enter end room", text: $endID)
                     .multilineTextAlignment(.center)
                 Button("Calculate") {
-                    let res = school.findPath(start: rooms[Int(startID) ?? 0], end: rooms[Int(endID) ?? 5])
+                    let start_int = Int(startID) ?? -1
+                    let end_int = Int(endID) ?? -1
+                    if (start_int < 10 || start_int > 19) || (end_int < 10 || end_int > 19) {
+                        inters = "Please enter a valid room number"
+                        return;
+                    }
+                    let res = school.findPath(start: rooms[start_int-10], end: rooms[end_int-10])
                     dist = res.dist
                     hallways = ""
                     for hall in res.halls {
@@ -33,53 +37,31 @@ struct ContentView: View {
                     hallways = "Hallway IDs (Reversed): " + hallways
                     inters = ""
                     for inter in res.inters {
-                        inters+=String(inter) + ", "
+                        inters+=String(inter + 1) + ", "
                     }
-                    inters = "Intersection IDs: " + inters
-                    print(school.halls)
+                    inters = "Intersection to cross: " + inters
+                    
                     
                 }
                 .frame(maxWidth: .infinity)
                 Image("Map_GPS")
                     .resizable()
                     .aspectRatio(contentMode: .fit)
-                Text(String(dist))
-                    .frame(maxWidth: .infinity, alignment: .center)
-                Text(hallways)
+                    .background(Color.white)
+                Text("Total Distance: " + String(dist))
                     .frame(maxWidth: .infinity, alignment: .center)
                 Text(inters)
                     .frame(maxWidth: .infinity, alignment: .center)
-        VStack(alignment: .leading) {
-            TextField("Enter start room (10-19)", text: $startID)
-            TextField("Enter end room (10-19)", text: $endID)
-            Button("Calculate") {
-                let start_int = Int(startID) ?? -1
-                let end_int = Int(endID) ?? -1
-                if (start_int < 10 || start_int > 19) || (end_int < 10 || end_int > 19) {
-                    inters = "Please enter a valid room number"
-                    return;
-                }
-                let res = school.findPath(start: rooms[start_int-10], end: rooms[end_int-10])
-                dist = res.dist
-                hallways = ""
-                for hall in res.halls {
-                    hallways+=String(hall.id) + ", "
-                }
-                hallways = "Hallway IDs (Reversed): " + hallways
-                inters = ""
-                for inter in res.inters {
-                    inters+=String(inter + 1) + ", "
-                }
-                inters = "Intersection to cross: " + inters
                 
             }
         }
-        
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // 1
+                .accentColor(Color.green)
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
