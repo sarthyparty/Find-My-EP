66539fe462710bfc1956c35ef376b9d3f7ef5a37
Sarthak Agrawal <agraw189@umn.edu>
2022-04-18 13:53:24 -0500

    something

--- Find My EP/ContentView.swift
+++ Find My EP/ContentView.swift
@@ -25,7 +25,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ScrollView {
-                
+  
                 SearchBar(searchBarText: "Enter start room number", searchText: $searchText, isSearching: $isSearching, isSearchingOther: $isSearching2)
                 
                 if (isSearching) {
