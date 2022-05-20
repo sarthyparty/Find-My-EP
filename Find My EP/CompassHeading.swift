eebd12b63b02526e0229d2e8d919cd96c412f2a0
Sarthak Agrawal <agraw189@umn.edu>
2022-05-20 10:53:44 -0500

    added latitude and longitude

--- Find My EP/CompassHeading.swift
+++ Find My EP/CompassHeading.swift
@@ -1,42 +1,62 @@
-//
+
 //  CompassHeading.swift
 //  Find My EP
 //
 //  Created by 64000774 on 4/27/22.
-//
 
-//import Foundation
-//import Combine
-//import CoreLocation
-//
-//class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
-//    var objectWillChange = PassthroughSubject<Void, Never>()
-//    var degrees: Double = .zero {
-//        didSet {
-//            objectWillChange.send()
-//        }
-//    }
-//
-//    private let locationManager: CLLocationManager
-//
-//    override init() {
-//        self.locationManager = CLLocationManager()
-//        super.init()
-//
-//        self.locationManager.delegate = self
-//        self.setup()
-//    }
-//
-//    private func setup() {
-//        self.locationManager.requestWhenInUseAuthorization()
-//
-//        if CLLocationManager.headingAvailable() {
-//            self.locationManager.startUpdatingLocation()
-//            self.locationManager.startUpdatingHeading()
-//        }
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
-//        self.degrees = -1 * newHeading.trueHeading
-//    }
-//}
+
+import Foundation
+import Combine
+import CoreLocation
+
+class CompassHeading: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var objectWillChange = PassthroughSubject<Void, Never>()
+    var degrees: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    var latitude: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    var longitude: Double = .zero {
+        didSet {
+            objectWillChange.send()
+        }
+    }
+    
+    private let locationManager: CLLocationManager
+    
+    override init() {
+        self.locationManager = CLLocationManager()
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.setup()
+    }
+    
+    private func setup() {
+        self.locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.headingAvailable() {
+            self.locationManager.startUpdatingLocation()
+            self.locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateHeading newHeading: CLHeading) {
+        self.degrees = -1 * newHeading.trueHeading
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let locationArray = locations as NSArray
+        let locationObj = locationArray.lastObject as! CLLocation
+        let coord = locationObj.coordinate
+        longitude = coord.longitude
+        latitude = coord.latitude
+    }
+}
